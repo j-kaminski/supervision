dd6264ed8e005c821e111b32f7248ddf42e1668b
pawko37 <domuradpawel01@gmail.com>
2022-10-22 21:49:14 +0200

    protocol checking corrections

--- protocol_checking.py
+++ protocol_checking.py
@@ -5,6 +5,6 @@ def protocol_checking(url):
     if '://' not in url:
         url = "http://" + url
     r = requests.get(url)
-    if 'https' in r.url:
+    if 'https' in r.url[:5]:
         return 0
     return 1
