d110592068375c16768f2d74ca0d910ed93954d4
j-kaminski <din0mite31@gmail.com>
2022-10-23 06:40:49 +0200

    update html cosine similarity, add new samples

--- html-similarity/main.py
+++ html-similarity/main.py
@@ -1,4 +1,6 @@
 import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import math
 import re
 from bs4 import BeautifulSoup
@@ -11,7 +13,13 @@ class HTMLSimilarity:
     HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0'}
 
     def __init__(self):
-        pass
+        self.driver = self._create_driver()
+
+    def _create_driver(self):
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        return driver
         
     def get_soup_text(self, dir_path, file_name):
         html_text = None
@@ -37,20 +45,64 @@ class HTMLSimilarity:
         else:
             return float(numerator) / denominator
 
+    def download_legit_domains(self):
+        dir_path = os.path.join(os.path.dirname(__file__), 'html_legit_domain_pages')
+
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        with open('domains.txt', 'r') as f:
+            urls = f.readlines()
+            for url in urls:
+                self.download_html(dir_path, url)
     
-    def download_html(self, url):
+    def download_html(self, dir_path, url):
         url_parsed = urlparse(url).netloc
-        with open(f'{url_parsed}.html', 'wb') as f:
-            r = requests.get(url, headers=self.HEADERS)
-            f.write(r.content) 
+        if not os.path.exists(os.path.join(dir_path, f'{url_parsed}.html')): 
+            with open(os.path.join(dir_path, f'{url_parsed}.html'), 'wb') as f:
+                    print('download', url_parsed)
+                    self.driver.get(url)
+                    content = self.driver.page_source
+                    # r = requests.get(url, headers=self.HEADERS)
+                    f.write(content.encode()) 
     
     def make_tokenizer(self):
         pass
+
+current_dir_path = os.path.dirname(__file__)
+
+h = HTMLSimilarity() 
+# scam = 'https://ca24credlt-agrlcole.top/offers.php'
+# path_to_scam = os.path.join(current_dir_path, urlparse(scam).netloc)
+# h.download_html(current_dir_path, scam)
+scam_html = 'millen.html'
+
+legit_dir = os.path.join(current_dir_path, 'html_legit_domain_pages')
+legit_html_list = [path for path in os.listdir(legit_dir)]
+
+
+results = []
+for html_file in legit_html_list:
+    legit_soup = h.get_soup_text(legit_dir, html_file)
+    scam_soup = h.get_soup_text(current_dir_path, scam_html)
+
+    filtered_text_legit = h.filtered_text(legit_soup)
+    filtered_text_scam = h.filtered_text(scam_soup)
+    result = h.get_cosine(Counter(filtered_text_legit), Counter(filtered_text_scam))
+    print(result, html_file)
+    results.append((result, html_file))
+
     
+# print(max(results))
+
+    
+
+
+# legit_dir = os.path.join(os.path.dirname(__file__))
+# scam_dir = os.path.join(os.path.dirname(__file__), 'scam_html')
+# h.download_html(scam_dir, scam)
 # scam stronka pobrać
 # 
-# h = HTMLSimilarity()
-# h.download_html('https://stackoverflow.com/questions/70414902/why-is-the-python-module-requests-downloading-the-html-page-instead-of-a-file')
 # current_dir = os.path.dirname(__file__)
 # agricole_dir = os.path.join(current_dir, 'agricole')
 # real_html_text = get_soup_text(current_dir, 'real_agricole.html') 
